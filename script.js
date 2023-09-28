2914b0adfbcc6c1d541a11837e04b1e7d6fff3a1
Marshmeller13 <66046595+Marshmeller13@users.noreply.github.com>
2023-09-28 16:51:21 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+var counter=0;
+function jump(){
+    if(character.classList == "animate"){return}
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },300);
+}
+
+// lets user jump with spacebar
+document.addEventListener('keyup', event => {
+    if (event.code === 'Space') {
+      jump();
+    }
+  })
+
+
+
+var checkDead = setInterval(function() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+        block.style.animation = "none";
+        alert("You Lost. Better Luck Next Time. Score: "+Math.floor(counter/100));
+        counter=0;
+        block.style.animation = "block 1s infinite linear";
+    }else{
+        counter++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+    }
+}, 10);
